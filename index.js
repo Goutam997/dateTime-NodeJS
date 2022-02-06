dd654965f0730b3b1abde504b9e4ad1d9f6f3c6d
Goutam Mohanty <30702929+Goutam997@users.noreply.github.com>
2022-02-07 00:25:11 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+let os = require("os");
+// console.log(os.hostname());
+
+// const http = require("http");
+// const port = 8000;
+
+// http.createServer((req,res) =>{
+//     res.writeHeader(200,{"Content-Type":"application/json"})
+    
+//     let data=[{
+//         user:"Najarajan"
+//     },{
+//         user:"Goutam"
+//     },{
+//         user:"Sumanth"
+//     },{
+//         user:"Punith",
+//         class:"10"
+//     }];
+//     res.write(JSON.stringify(data));
+//     res.end();
+// }).listen(port,() =>{
+//     console.log("Listening to ",port);
+// })
+
+const http = require("http");
+const port = 8000;
+const fs = require("fs");
+let datetime = new Date().toDateString();
+
+http.createServer((req,res) => {
+
+    res.writeHeader(200,{"Content-Type":"text/html"});
+
+    fs.writeFileSync(`${datetime}.txt`,datetime);
+    fs.readFile("sample.txt", (err,data)=>{
+        console.log(data);
+        res.write(data);
+        res.end();
+    })
+
+}).listen(port, () => {
+    console.log("Listening to ",port);
+})
